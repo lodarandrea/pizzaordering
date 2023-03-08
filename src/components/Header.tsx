cbaf246a27826bc650297ecb514fcb6fe52d3bbe
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-03-08 10:58:28 +0100

    configuring the app

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../img/logo.jpg'
+
+function Header() {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default Header
