81a57fcf495158b61fc834ed21dac2c0f44dfef9
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-03-08 14:00:27 +0100

    implementing Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
 import logo from '../img/logo.jpg'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
+  const [open, setOpen] = useState(false)
+
+  const navi = [
+    { name: 'Home', url: '/' },
+    { name: 'Menu', url: '/menu' },
+    { name: 'Contact Us', url: '/contact' },
+  ]
   return (
-    <div>
+    <div className="flex justify-between">
       <img src={logo} alt="logo" />
+      <button
+        onClick={() => {
+          setOpen(!open)
+        }}
+      >
+        <MenuIcon fontSize="large" />
+      </button>
+      <div className={open ? 'flex flex-col' : 'hidden'}>
+        <div>
+          {navi.map((naviItem) => (
+            <Link to={`${naviItem.url}`} key={naviItem.name}>
+              {naviItem.name}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
