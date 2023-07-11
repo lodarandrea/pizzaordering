79b4cbc650a46953ea822a86950d7e70bf5b0d06
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-07-11 10:54:22 +0200

    implementing Redux-Persist on the Store

--- src/components/buttons/CheckoutButton.tsx
+++ src/components/buttons/CheckoutButton.tsx
@@ -2,7 +2,7 @@ function CheckoutButton() {
   return (
     <div className="my-10">
       <button className="bg-indigo-700 text-xl text-white rounded-lg px-5 py-3 hover:bg-indigo-800 hover:shadow-lg hover:shadow-indigo-900">
-        Order Now
+        Checkout
       </button>
     </div>
   )
